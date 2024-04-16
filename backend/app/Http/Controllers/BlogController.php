1999ebc1af74195911560cf467557a189e05ab36
darshan45672 <drshnbhandary@gmail.com>
2024-04-16 14:16:22 +0530

    uploaded images moves to public file

--- backend/app/Http/Controllers/BlogController.php
+++ backend/app/Http/Controllers/BlogController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use App\Models\TempImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
 
 class BlogController extends Controller
@@ -13,17 +15,19 @@ class BlogController extends Controller
         return view('blogs.index');
     }
 
-    public function show(){
+    public function show()
+    {
         return view('blogs.show');
     }
-    public function store( Request $request){
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'title' => 'required | min:3',
             'description' => 'required',
             'author' => 'required | min:3',
         ]);
         if ($validator->fails()) {
-            return response()->json([ 'status' => false, 'message'=>'check the errors', 'error' => $validator->errors()]);
+            return response()->json(['status' => false, 'message' => 'check the errors', 'error' => $validator->errors()]);
         }
 
         $blog = new Blog();
@@ -34,11 +38,35 @@ class BlogController extends Controller
         // $blog->image = $request->image;
         $blog->save();
 
-        return response()->json([ 'status' => true, 'message'=>'Blog added successfully', 'data' => $blog]);
+        $tempImage = TempImage::find($request->image_id);
 
+        if ($tempImage != null) {
+
+            // 1713255034.png
+            $imageExtensionArray = explode('.', $tempImage->name);
+            $extension = last($imageExtensionArray);
+
+            $imageName = time() . '-' . $blog->id . '.' . $extension;
+
+            $blog->image = $imageName;
+            $blog->save();
+
+            $sourcePath = public_path('uploads/temp/'.$tempImage->name);
+            $destinationPath = public_path('uploads/blogs/'.$imageName);
+            dd($destinationPath);
+            File::copy( $sourcePath, $destinationPath);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Blog added successfully',
+            'data' => $blog
+        ]);
     }
-    public function update(){
+    public function update()
+    {
     }
-    public function destroy(){
+    public function destroy()
+    {
     }
 }
